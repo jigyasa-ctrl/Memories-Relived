a79cc9df5a376a05b2243a00ec87481524aec1d3
jigyasa-ctrl <jigyasaupadhyay60@gmail.com>
2021-07-10 19:11:35 +0530

    Deploy

--- server/index.js
+++ server/index.js
@@ -2,12 +2,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors'; 
-
+import dotenv from 'dotenv'
 import router from './routes/posts.js'
 
 
 
 const app = express();
+dotenv.config()
 //setting up body parser to populate sender request
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
@@ -15,7 +16,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
 app.use('/posts', router)
 
-const CONNECTION_URL ='mongodb+srv://jigyasauser:jigyasauserPassword@cluster0.xbi7m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const CONNECTION_URL = process.env.CONNECTION_URL
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true })
